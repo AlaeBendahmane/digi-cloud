eec673f834749d435b1d0cebde7816ac9ea61fc8
AlaeBendahmane <alaebendahmane1@gmail.com>
2024-01-09 17:03:24 +0100

    modified:   src/components/nodata/index.tsx

--- src/components/nodata/index.tsx
+++ src/components/nodata/index.tsx
@@ -2,7 +2,7 @@ import Notdata from "../../assets/icons/not-data.svg";
 export default function index() {
     return (
         <div className="flex  h-full  min-h-[50vh] flex-col items-center justify-center gap-[4rem] text-4xl mt-20">
-            <img src={Notdata} alt="" className="h-[20rem]" />
+            <img src={Notdata} alt="" className="h-[25rem]" />
             <span className=" font-extrabold text-purple-600 " >
                 No Data found
             </span>
