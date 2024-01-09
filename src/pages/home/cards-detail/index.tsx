dce68e441fbe23064a5337df82c8c47801386d89
AlaeBendahmane <alaebendahmane1@gmail.com>
2024-01-09 17:51:52 +0100

    new file:   src/assets/icons/offline.svg 	renamed:    src/assets/unused/online.svg -> src/assets/icons/online.svg 	renamed:    src/assets/unused/rooms.svg -> src/assets/icons/rooms.svg 	deleted:    src/assets/unused/home-owner.svg 	deleted:    src/assets/unused/leak-alert.svg 	deleted:    src/assets/unused/lock-alert.svg 	deleted:    src/assets/unused/offline.svg 	modified:   src/pages/home/cards-detail/index.tsx

--- src/pages/home/cards-detail/index.tsx
+++ src/pages/home/cards-detail/index.tsx
@@ -1,41 +1,42 @@
 import { Card } from "@material-tailwind/react";
 import React from "react";
-import { ReactComponent as NbrDevice } from "../../../assets/icons/nbr-device.svg";
-import { ReactComponent as HomeDashboard } from "../../../assets/icons/home-dashbord.svg";
-import { ReactComponent as AlertComponent } from "../../../assets/icons/alerts.svg";
+import Online from '../../../assets/icons/online.svg'
+import Offline from '../../../assets/icons/offline.svg'
+import HomeDashboard from "../../../assets/icons/rooms.svg";
+import AlertComponent from "../../../assets/icons/alerts.svg";
 function CardsDetail() {
   return (
     <React.Fragment>
-      <Card className="min-h-16 items-center p-4 md:col-span-3">
+      <Card className="min-h-16 items-center p-4 md:col-span-3" placeholder={undefined}>
         <div className="flex h-full w-full" >
-          <NbrDevice className="fill-[#3FBC58]" />
+          <img src={Online} alt="" />
           <div className="flex flex-col items-stretch gap-1 ml-3">
             <span className="text-4xl">179</span>
             <span>Online Devices</span>
           </div>
         </div>
       </Card>
-      <Card className="min-h-16 p-4 md:col-span-3">
+      <Card className="min-h-16 p-4 md:col-span-3" placeholder={undefined}>
         <div className="flex h-full  w-full ">
-          <NbrDevice className="fill-[#5D7285]" />
+          <img src={Offline} alt="" />
           <div className="flex flex-col items-stretch gap-1 ml-3">
             <span className="text-4xl">179</span>
             <span>Offline Devices</span>
           </div>
         </div>
       </Card>
-      <Card className="min-h-16 p-4 md:col-span-3">
+      <Card className="min-h-16 p-4 md:col-span-3" placeholder={undefined}>
         <div className="flex h-full  w-full ">
-          <HomeDashboard className="fill-[#9C179E]" />
+          <img src={HomeDashboard} alt="" />
           <div className="flex flex-col items-stretch gap-1 ml-3">
             <span className="text-4xl">179</span>
             <span>Total Rooms</span>
           </div>
         </div>
       </Card>
-      <Card className="min-h-16 p-4 md:col-span-3">
+      <Card className="min-h-16 p-4 md:col-span-3" placeholder={undefined}>
         <div className="flex h-full  w-full ">
-          <AlertComponent />
+          <img src={AlertComponent} alt="" />
           <div className="flex flex-col items-stretch gap-1 ml-3">
             <span className="text-4xl">179</span>
             <span>Alerts</span>
