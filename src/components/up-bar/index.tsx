7f00c88f21068d8437d7a84d15c5a3ada6a7938a
AlaeBendahmane <alaebendahmane1@gmail.com>
2024-01-26 17:45:22 +0100

    modified:   src/components/up-bar/index.tsx 	modified:   src/main.tsx

--- src/components/up-bar/index.tsx
+++ src/components/up-bar/index.tsx
@@ -4,16 +4,19 @@ import ProfileMenu from "./profile-menu";
 import ToggleFullScreen from "./toggle-full-screen";
 import Logo from '../../assets/icons/logo.svg'
 import Lang from "./lang";
+import { Link } from "react-router-dom";
 function Upbar() {
   return (
     <div className="fixed z-[500] top-0 left-0 right-0 ">
       <Navbar className="mx-auto !h-14 max-w-[100%] p-2 lg:px-4 rounded-none" placeholder={undefined}>
         <div className="relative mx-auto flex items-center text-blue-gray-900">
-          <img src={Logo} alt="" className="h-10" />{/*"/logo.svg" */}
+          <Link to={'#'}>
+            <img src={Logo} alt="" className="h-10" />
+          </Link>
           <div className="mx-auto"></div>
           <div className="space-x-1 mr-1">
             <Lang />
-            <Notifications />
+            {/*<Notifications />*/}
             <ToggleFullScreen />
           </div>
           <ProfileMenu />
